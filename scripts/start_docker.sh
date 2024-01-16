41f34b2299b2dec4bbfd8b41f6e0f2bb118aaf56
yourzinc <your_zinc@ewhain.net>
2024-01-16 15:24:58 +0900

    fix: container conflict

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # start_docker.sh
+
 cd /home/ec2-user/my-nginx-app
+
 docker build -t my-nginx-image .
+
+if docker ps -a | grep -q 'my-nginx-container'; then
+    echo "Container 'my-nginx-container' already exists. Stopping and removing it."
+    docker stop my-nginx-container
+    docker rm my-nginx-container
+fi
+
 docker run -d --name my-nginx-container -p 80:80 my-nginx-image
