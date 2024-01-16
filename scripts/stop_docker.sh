61d3a1886978cf7a482caf2470b0cec17ac65479
yourzinc <your_zinc@ewhain.net>
2024-01-16 19:21:38 +0900

    fix: logging env

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 # stop_docker.sh
+echo "Stopping container: ${CONTAINER_NAME}"
 docker stop ${CONTAINER_NAME}
+if [ $? -eq 0 ]; then
+    echo "Container ${CONTAINER_NAME} stopped successfully."
+else
+    echo "Failed to stop container ${CONTAINER_NAME}."
+fi
+
+echo "Removing container: ${CONTAINER_NAME}"
 docker rm ${CONTAINER_NAME}
+if [ $? -eq 0 ]; then
+    echo "Container ${CONTAINER_NAME} removed successfully."
+else
+    echo "Failed to remove container ${CONTAINER_NAME}."
+fi
