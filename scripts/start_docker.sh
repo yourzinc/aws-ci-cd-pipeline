d13806b32babcf2b4f1409dbde2c60f82b2d5cc5
yourzinc <your_zinc@ewhain.net>
2024-01-17 14:09:18 +0900

    fix: ecr login

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -2,10 +2,12 @@
 # start_docker.sh
 
 IMAGE_NAME="<ECR_REGISTRY>/<ECR_REPOSITORY>:<IMAGE_TAG>"
+AWS_REGION="<AWS_REGION>"
+ECR_REGISTRY="<ECR_REGISTRY>"
 
-$(aws ecr get-login --no-include-email --region <AWS_REGION>)
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
 
-docker pull ${IMAGE_NAME}
+docker pull $IMAGE_NAME
 
 if docker ps -a | grep -q "<CONTAINER_NAME>"; then
     echo "Container '<CONTAINER_NAME>' already exists. Stopping and removing it."
@@ -13,4 +15,4 @@ if docker ps -a | grep -q "<CONTAINER_NAME>"; then
     docker rm <CONTAINER_NAME>
 fi
 
-docker run -d --name <CONTAINER_NAME> -p 8080:8080 ${IMAGE_NAME}
+docker run -d --name <CONTAINER_NAME> -p 8080:8080 $IMAGE_NAME
